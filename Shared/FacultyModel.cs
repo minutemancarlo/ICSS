e340304a28d325ddc56411a95dcd72691229a6a6
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2025-01-31 02:32:59 +0800

    Added Faculty Model

---
+++ Shared/FacultyModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSS.Shared
+{
+    public class FacultyModel : BaseModel
+    {
+        public int? FacultyId { get; set; }
+        public string FacultyName { get; set; }
+        public string AcademicRank { get; set; }
+        public int? DepartmentId { get; set; }
+        public string? BachelorsDegree { get; set; }
+        public string? MastersDegree { get; set; }
+        public string? DoctorateDegree { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
+
