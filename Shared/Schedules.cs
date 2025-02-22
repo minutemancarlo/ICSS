031bb76dd961fc45e2fc1bc93b4caf10d4deb09b
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2025-02-22 18:43:51 +0800

    Added Schedule Model

---
+++ Shared/Schedules.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICSS.Shared
+{
+    public class ScheduleRequest : BaseModel
+    {
+        public int? TaskId { get; set; }
+        public TaskStatus Status { get; set; }
+        public TaskType TaskType { get; set; }        
+        public Course? Course { get; set; }
+        
+        
+
+    }
+}
